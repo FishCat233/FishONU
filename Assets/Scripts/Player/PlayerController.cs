6e7cc53d3e93928565096c4ad59bc224038a4b6a
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-09 12:35:36 +0800

    feat(player): 添加和完善了自动入座策略，会自动选空位入座

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,8 @@ namespace FishONU.Player
         [SerializeField, HideInInspector] private OwnerInventory ownerInventory;
         [SerializeField, HideInInspector] private SecretInventory secretInventory;
 
+        // TODO: 智能入座
+
         [SyncVar(hook = nameof(OnSeatIndexChange))]
         public int seatIndex;
 
@@ -45,27 +47,80 @@ namespace FishONU.Player
                 PositionOffset = new(0.65f, 0.1f, 0f),
                 RotationOffset = new(0f, 0f, -5f)
             };
+
+            if (isServer)
+            {
+                SitDown();
+            }
         }
 
-        public override void OnStartLocalPlayer()
+        public override void OnStopClient()
         {
-            base.OnStartLocalPlayer();
+            base.OnStopClient();
 
-            CmdSitDown();
+            if (isServer)
+            {
+                StandUp();
+            }
+        }
+
+
+        [Server]
+        public void SitDown()
+        {
+            var table = Object.FindFirstObjectByType<TableManager>();
+
+            if (table == null)
+            {
+                Debug.Log("TableManager is null");
+                return;
+            }
+
+            var assignedSeat = table.RequestSitDown(netIdentity);
+
+            if (assignedSeat != -1)
+            {
+                seatIndex = assignedSeat;
+                Debug.Log($"Server assigned seat {assignedSeat} to {gameObject.name}");
+            }
+            else
+            {
+                Debug.Log($"Server failed to assign seat to {gameObject.name}");
+            }
+        }
+
+        [Server]
+        public void StandUp()
+        {
+            var table = Object.FindFirstObjectByType<TableManager>();
+
+            if (table == null)
+            {
+                Debug.Log("TableManager is null");
+                return;
+            }
+
+            if (table.RequestStandUp(netIdentity) != -1)
+            {
+                Debug.Log($"Server try to stand up {gameObject.name} successfully");
+            }
+            else
+            {
+                Debug.Log($"Server try to stand up {gameObject.name}");
+            }
         }
 
         [Command]
         public void CmdSitDown()
         {
-            seatIndex = GameObject.FindGameObjectsWithTag("Player").Length - 1;
-
-            Debug.Log($"player {gameObject.name} command to sit at {seatIndex}");
+            SitDown();
         }
+
 
         [Command]
         public void CmdStandUp()
         {
-            // TODO:
+            StandUp();
         }
 
         [Client]
